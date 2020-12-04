eed7e5cd0fe75d34c427b8e15dd2dce938f1b844
Darrel Ross <darrelross314@gmail.com>
2020-12-03 20:42:36 -0700

    Initial Commit

--- src/main/java/com/example/hellojenkins/HelloController.java
+++ src/main/java/com/example/hellojenkins/HelloController.java
@@ -1,12 +1,23 @@
 package com.example.hellojenkins;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class HelloController {
-    public String hello() {
-        return "Hello Darrel";
+
+    private final String message;
+
+    public HelloController( @Value("${hello.message}") String message) {
+        this.message = message;
+    }
+
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello " + message);
     }
 }
